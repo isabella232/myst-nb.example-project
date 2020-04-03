1bb1bf668fbeef838c6a6ebb02262f9c7162ae04
Matt McKay <mamckay@gmail.com>
2020-04-03 11:03:55 +1100

    remove myst-parser to remove conflict

--- conf.py
+++ conf.py
@@ -27,7 +27,7 @@ author = 'Executable Book Project'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["myst_parser", "myst_nb", "sphinxcontrib.bibtex"]
+extensions = ["myst_nb", "sphinx_togglebutton", "sphinxcontrib.bibtex"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
