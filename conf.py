9149bb148bdc7a9686bc148712d8272b54ec74ce
Matt McKay <mamckay@gmail.com>
2020-04-07 15:51:35 +1000

    turn execution off

--- conf.py
+++ conf.py
@@ -50,3 +50,7 @@ html_logo = "_static/logo.png"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# -- Options for myst-nb
+
+jupyter_execute_notebooks = "off"
